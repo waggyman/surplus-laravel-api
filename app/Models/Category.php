481a686938148876eea278260a5557e79b3bdc48
waggyman <mas.awe.awe@gmail.com>
2023-03-21 21:44:58 +0700

    feature-add-category-seeder

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -14,6 +14,6 @@ class Category extends Model
 
     public function products (): BelongsToMany
     {
-        return $this->belongsToMany(Category::class, 'category_product', 'category_id', 'product_id');
+        return $this->belongsToMany(Product::class, 'category_product', 'category_id', 'product_id');
     }
 }
--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
 
 class CategorySeeder extends Seeder
 {
@@ -12,6 +13,23 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $categories = [
+            [
+                'name' => 'hat',
+                'enable' => true
+            ],
+            [
+                'name' => 'bottle',
+                'enable' => true
+            ],
+            [
+                'name' => 'top',
+                'enable' => true
+            ]
+        ];
+        foreach ($categories as $category) {
+            $newCategory = Category::create($category);
+            $newCategory->products()->attach($newCategory->id);
+        }
     }
 }
