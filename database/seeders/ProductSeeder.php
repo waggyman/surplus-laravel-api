e3777c99995c6fd629be31283a3216b60f7df427
waggyman <mas.awe.awe@gmail.com>
2023-03-21 21:45:24 +0700

    feature-add-product-seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Product;
 
 class ProductSeeder extends Seeder
 {
@@ -12,6 +13,36 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $products = [
+            [
+                "name" => "Energenic Hat",
+                "description" => "Hat that can increase your Energy. Boost it up!",
+                "enable" => true
+            ],
+            [
+                "name" => "Energenic Bottle",
+                "description" => "Bottle that can increase your Energy. Boost it up!",
+                "enable" => true
+            ],
+            [
+                "name" => "Energenic Shirt",
+                "description" => "Shirt that can increase your Energy. Boost it up!",
+                "enable" => true
+            ],
+            [
+                "name" => "Energenic Socks",
+                "description" => "Socks that can increase your Energy. Boost it up!",
+                "enable" => true
+            ],
+            [
+                "name" => "Energenic Bike",
+                "description" => "Bike that can increase your Energy. Boost it up!",
+                "enable" => true
+            ]
+        ];
+        
+        foreach ($products as $product) {
+            Product::create($product);
+        }
     }
 }
