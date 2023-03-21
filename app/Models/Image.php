f9a4afa708bfdf77b458edd1fb73206bac8d909b
waggyman <mas.awe.awe@gmail.com>
2023-03-21 22:06:17 +0700

    feature-list-product

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Image extends Model
 {
@@ -15,5 +16,12 @@ class Image extends Model
     public function products (): BelongsToMany
     {
         return $this->belongsToMany(Product::class, 'product_image', 'image_id', 'product_id');
+    }
+
+    protected function file(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => asset($value)
+        );
     }
 }
