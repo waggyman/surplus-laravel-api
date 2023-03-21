83f62326bf34018c603d7b17d7c4871eeb0958ba
waggyman <mas.awe.awe@gmail.com>
2023-03-21 21:44:34 +0700

    feature-add-image-seeder

--- database/seeders/ImageSeeder.php
+++ database/seeders/ImageSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Image;
 
 class ImageSeeder extends Seeder
 {
@@ -12,6 +13,37 @@ class ImageSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $images = [
+            [
+                'name' => 'hat',
+                'file' => '/images/hat.jpg',
+                'enable' => true
+            ],
+            [
+                'name' => 'bottle',
+                'file' => '/images/bottle.jpg',
+                'enable' => true
+            ],
+            [
+                'name' => 'shirt',
+                'file' => '/images/shirt.jpg',
+                'enable' => true
+            ],
+            [
+                'name' => 'sock',
+                'file' => '/images/sock.jpg',
+                'enable' => true
+            ],
+            [
+                'name' => 'bike',
+                'file' => '/images/bike.jpg',
+                'enable' => true
+            ],
+        ];
+
+        foreach ($images as $image) {
+            $newImg = Image::create($image);
+            $newImg->products()->attach($newImg->id);
+        }
     }
 }
